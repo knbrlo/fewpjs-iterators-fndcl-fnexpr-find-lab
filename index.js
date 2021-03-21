c2fbc3875c7d9928e6ea39d047e97d2a19ff0ed5
Ken Barlow <kenbrlow@gmail.com>
2021-03-20 22:52:24 -0600

    commit tree c0acbde715d01d89589214c52467920f32ca4f0c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+] 
 
+function superbowlWin(arrayToCheck) {
+    let yearWon;
+    let item = arrayToCheck.find(function(e) {
+        return e.result === "W"
+    });
+
+    if (item != null) {
+        yearWon = item.year;
+    }
+    
+    return yearWon;
+}
